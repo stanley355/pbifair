c6abcd2da2e8ab9164231de6c50946022f728127
stanley355 <winatastanley355@gmail.com>
2022-04-25 20:52:39 +0700

    dependencies

---
+++ pages/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <header className={styles.navbar}>
+      <h3>PBI UAJ</h3>
+    </header>
+  );
+}
+
+export default Navbar;
