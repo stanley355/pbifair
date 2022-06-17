c8bda95d8a1f49df9941e38ed63a8ad9b0a212ab
Stanley Winata <winatastanley355@gmail.com>
2022-06-17 09:35:19 +0700

    quest 5

---
+++ pages/quest-6/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PassForm from '../../components/PassForm';
+import styles from './quest6.module.scss';
+import instructionStyle from '../../styles/pages/instruction.module.scss';
+
+const Quest6 = () => {
+  const Instruction = () => {
+    return (
+      <div className={instructionStyle.instructions}>
+        <h3>Draw Me</h3>
+        <div className={instructionStyle.instructions__subtitle}>Rules/ Guidelines:</div>
+        <div>
+          Committee hasn't given the instruction yet, 
+          continue to next step with 'instruction' as the password
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className={styles.quest4}>
+        <Instruction />
+        <PassForm
+          password="instruction"
+          nextURL='/quest-7/'
+          level={6}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Quest6;
